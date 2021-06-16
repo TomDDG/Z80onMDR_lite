69c05be470496c3aac5989165c635143bb69fa3a
TomDDG <tom.dalby@btinternet.com>
2021-06-16 23:17:43 +0100

    Update Z80onMDR_Lite.c

--- Z80onMDR_Lite.c
+++ Z80onMDR_Lite.c
@@ -1,19 +1,20 @@
 // Z80onMDR_Lite - Z80 snapshot to Microdrive MDR image converter
 // a cut down version of the full Z80onMDR to use with or within other utilities 
-// Copyright (c) 2021, Tom Dalby
 // 
-// Z80onMDR_Lite is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-// 
-// Z80onMDR_Lite is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-// GNU General Public License for more details.
-// 
-// You should have received a copy of the GNU General Public License
-// along with Z80onMDR_Lite. If not, see <http://www.gnu.org/licenses/>.
+// Copyright (C) 2021 Tom Dalby
+//
+//    This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 //
 // usage: z80onmdr_lite snapshot.z80 
 //   this will create a mdr cartridge image called snapshot.mdr
